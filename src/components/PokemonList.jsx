36580f849a9dcd528f93b2b3fbaf5b444f831ddd
AntonBrito <tonybrito85@gmail.com>
2018-07-01 00:39:12 -0400

    Able to get listCount

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 class PokemonList extends Component {
+    constructor() {
+        super()
+    }
+    //-----------Message
+    renderPokemons(){
+        let counter = 0;
 
+        return this.props.pokemonResult.map( pokemons => {
+            console.log(pokemons);
+            counter = counter +1;
+
+            return <li key={counter}className="card">name: {pokemons.pokemon.name} <br/> url: {pokemons.pokemon.url}</li>
+
+            // return <Pokemon key={counter} count{counter}/>
+        })
+    }
+    
+    //-------------
+
+    
     render(){
         // console.log(this.props)
         if ( this.props.pokemonResult.length > 0) {
